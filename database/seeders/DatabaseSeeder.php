c7a75ed0e007521999a7a3941d91ea1f3f37d4b5
Marco Rieser <marco@rieser.dev>
2024-03-04 10:51:38 +0100

    checkbox list

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Speaker;
 use App\Models\User;
 use App\Models\Venue;
 use Illuminate\Database\Seeder;
@@ -22,6 +23,7 @@ class DatabaseSeeder extends Seeder
         ]);
 
         Venue::factory(200)->create();
+        Speaker::factory(20)->create();
 
         // \App\Models\User::factory(10)->create();
 
